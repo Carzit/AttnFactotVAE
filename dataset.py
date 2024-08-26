d11868b40e97279223a3d12276be9a120ec0f128
Carzit <126076978+Carzit@users.noreply.github.com>
2024-08-26 14:33:04 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,306 @@
+import os
+import sys
+import random
+import datetime
+import logging
+import argparse
+from numbers import Number
+from typing import List, Dict, Tuple, Optional, Literal, Union, Any, Callable
+
+from functools import lru_cache
+
+import torch
+from torch.utils.data import Dataset, Sampler
+import pandas as pd
+import numpy as np
+
+def save_dataframe(df:pd.DataFrame, path:str, format:Literal["csv", "pkl", "parquet", "feather"])->None:
+    if format == "csv":
+        df.to_csv(path)
+    elif format ==  "pkl":
+        df.to_pickle(path)
+    elif format == "parquet":
+        df.to_parquet(path)
+    elif format == "feather":
+        df.to_feather(path)
+    else:
+        raise NotImplementedError()
+
+def load_dataframe(path:str, format:Literal["csv", "pkl", "parquet", "feather"])->pd.DataFrame:
+    if format == "csv":
+        df = pd.read_csv(path, index_col=0)
+    elif format ==  "pkl":
+        df = pd.read_pickle(path)
+    elif format == "parquet":
+        df = pd.read_parquet(path)
+    elif format == "feather":
+        df = pd.read_feather(path)
+    else:
+        raise NotImplementedError()
+    return df
+
+def translate_dtype(dtype:Literal["FP32", "FP64", "FP16", "BF16"]) -> torch.dtype:
+    if dtype == "FP32":
+        return torch.float32
+    elif dtype == "FP64":
+        return torch.float64
+    elif dtype == "FP16":
+        return torch.float16
+    elif dtype == "BF16":
+        return torch.bfloat16
+
+@torch.no_grad()
+def drop_nan_inf(tensor:torch.Tensor) -> Tuple[torch.Tensor]:
+    mask = ~torch.any(torch.isnan(tensor) | torch.isinf(tensor), dim=[0,2])
+    filtered_tensor = tensor[:,mask,:]
+    return filtered_tensor, mask
+
+@torch.no_grad()
+def convert_nan_inf(tensor:torch.Tensor) -> torch.Tensor:
+    converted_tensor = tensor.nan_to_num()
+    return converted_tensor
+
+class StockDataset(Dataset):
+    """继承自 Dataset，用于加载和预处理股票数据集。支持序列化分割和随机分割，并提供了数据集的信息。"""
+    def __init__(self, 
+                 quantity_price_feature_dir:str, 
+                 fundamental_feature_dir:str,
+                 label_dir:str, 
+                 label_name:str, 
+                 format:Literal["csv", "pkl", "parquet", "feather"] = "pkl",
+                 cache_size:int = 10, 
+                 dtype:Literal["FP32", "FP64", "FP16", "BF16"] = "FP32") -> None:
+        super().__init__()
+        self.quantity_price_feature_dir:str = quantity_price_feature_dir
+        self.fundamental_feature_dir:str = fundamental_feature_dir
+        self.label_dir:str = label_dir
+        
+        self.format:str = format
+        self.label_name:str = label_name
+
+        self.quantity_price_feature_file_paths:List[str] = [os.path.join(quantity_price_feature_dir, f) for f in os.listdir(quantity_price_feature_dir) if f.endswith(format)]
+        self.fundamental_feature_file_paths:List[str] = [os.path.join(fundamental_feature_dir, f) for f in os.listdir(fundamental_feature_dir) if f.endswith(format)]
+        self.label_file_paths:List[str] = [os.path.join(label_dir, f) for f in os.listdir(label_dir) if f.endswith(format)]
+
+        self.cache_size:int = cache_size
+        self.dtype:torch.dtype = translate_dtype(dtype=dtype)
+        #self.load_df:Callable = self.create_cached_loader() 
+
+    @lru_cache(maxsize=10)
+    def load_dataframe(self, path:str, format:Literal["csv", "pkl", "parquet", "feather"])->pd.DataFrame:
+        if format == "csv":
+            df = pd.read_csv(path, index_col=0)
+        elif format ==  "pkl":
+            df = pd.read_pickle(path)
+        elif format == "parquet":
+            df = pd.read_parquet(path)
+        elif format == "feather":
+            df = pd.read_feather(path)
+        else:
+            raise NotImplementedError()
+        return df
+
+    def __getitem__(self, index) -> Tuple[torch.Tensor]:
+        """根据索引 index 从文件路径列表中读取对应的 CSV 文件。将读取的数据转换为 PyTorch 张量，并处理 NaN 值。
+        注意，这是一种惰性获取数据的协议，数据集类并不事先将所有数据载入内存，而是根据索引再做读取。能较好地应对大量数据的情形，节约内存；但训练时由于IO瓶颈速度较慢，需要花费大量时间在csv的读取上面。"""
+        quantity_price_feature = self.load_dataframe(self.quantity_price_feature_file_paths[index], format=self.format).iloc[:,1:]
+        quantity_price_feature = torch.from_numpy(quantity_price_feature.values).to(self.dtype)
+        
+        fundamental_feature = self.load_dataframe(self.fundamental_feature_file_paths[index], format=self.format).iloc[:,1:]
+        fundamental_feature = torch.from_numpy(fundamental_feature.values).to(self.dtype)
+
+        label = self.load_dataframe(self.label_file_paths[index], format=self.format).iloc[:,1:][self.label_name]
+        label = torch.from_numpy(label.values).to(self.dtype)
+        return quantity_price_feature, fundamental_feature, label
+    
+    def __len__(self):          
+        """获取数据集长度"""
+        return min(len(self.quantity_price_feature_file_paths), 
+                   len(self.fundamental_feature_file_paths), 
+                   len(self.label_file_paths))
+    
+    def serial_split(self, ratios:List[Number], mask:int=0) -> List["StockDataset"]:
+        """序列化分割。根据给定的比例 ratios 将数据集分割成多个子数据集，且保持原顺序。返回一个StockDataset类的列表。"""
+        total_length = len(self)
+        split_lengths = list(map(lambda x:round(x / sum(ratios) * total_length), ratios))
+        split_lengths[0] = total_length - sum(split_lengths[1:])
+        splitted_datasets = []
+
+        i = 0
+        for j in split_lengths:
+            splitted_dataset = StockDataset(quantity_price_feature_dir=self.quantity_price_feature_dir, 
+                                            fundamental_feature_dir=self.fundamental_feature_dir,
+                                            label_dir=self.label_dir, 
+                                            label_name=self.label_name, 
+                                            format=self.format, 
+                                            cache_size=self.cache_size)
+            splitted_dataset.quantity_price_feature_file_paths = splitted_dataset.quantity_price_feature_file_paths[i:i+j]
+            splitted_dataset.fundamental_feature_file_paths = splitted_dataset.fundamental_feature_file_paths[i:i+j]
+            splitted_dataset.label_file_paths = splitted_dataset.label_file_paths[i:i+j]
+            splitted_datasets.append(splitted_dataset)
+            i += (j+mask)
+
+        return splitted_datasets
+    
+    def random_split(self, ratios:List[Number]) -> List["StockDataset"]:
+        """随机分割。根据给定的比例 ratios 将数据集分割成多个子数据集，且顺序随机。返回一个StockDataset类的列表。"""
+        total_length = len(self)
+        split_lengths = list(map(lambda x:round(x / sum(ratios) * total_length), ratios))
+        split_lengths[0] = total_length - sum(split_lengths[1:])
+        splitted_datasets = []
+
+        base_names = [os.path.basename(file_path) for file_path in self.quantity_price_feature_file_paths]
+        random.shuffle(base_names)
+        shuffled_quantity_price_feature_file_paths = [os.path.join(self.quantity_price_feature_dir, base_name) for base_name in base_names]
+        shuffled_fundamental_feature_file_paths = [os.path.join(self.fundamental_feature_dir, base_name) for base_name in base_names]
+        shuffled_label_file_paths = [os.path.join(self.label_dir, base_name) for base_name in base_names]
+
+        i = 0
+        for j in split_lengths:
+            splitted_dataset = StockDataset(quantity_price_feature_dir=self.quantity_price_feature_dir, 
+                                            fundamental_feature_dir=self.fundamental_feature_dir,
+                                            label_dir=self.label_dir, 
+                                            label_name=self.label_name, 
+                                            format=self.format, 
+                                            cache_size=self.cache_size)
+            splitted_dataset.quantity_price_feature_file_paths = shuffled_quantity_price_feature_file_paths[i:i+j]
+            splitted_dataset.fundamental_feature_file_paths = shuffled_fundamental_feature_file_paths[i:i+j]
+            splitted_dataset.label_file_paths = shuffled_label_file_paths[i:i+j]
+            splitted_datasets.append(splitted_dataset)
+            i += j
+            
+        return splitted_datasets
+
+class StockSequenceDataset(Dataset):
+    """StockSequenceDataset 类继承自 Dataset，将 StockDataset 中的单日数据堆叠为日期序列数据"""
+    def __init__(self, 
+                 stock_dataset:StockDataset, 
+                 seq_len:int,
+                 mode:Literal["convert", "drop"] = "convert") -> None:
+        super().__init__()
+        self.stock_dataset:StockDataset = stock_dataset # 原StockDataset对象
+        self.seq_len:int = seq_len # 日期序列长度
+        self.mode:str = mode
+
+    def __len__(self):
+        """获取数据集长度"""
+        return len(self.stock_dataset) - self.seq_len + 1
+    
+    def __getitem__(self, index) -> Tuple[torch.Tensor]:
+        """根据索引 index 从 StockDataset 中获取一个长度为 seq_len 的序列数据。"""
+        quantity_price_feature = torch.stack([self.stock_dataset[i][0] for i in range(index, index+self.seq_len)], dim=0) # (seq_len, num_stock, num_feature1)
+        fundamental_feature = self.stock_dataset[index+self.seq_len-1][1] # (num_stock, num_feature2)
+        label = self.stock_dataset[index+self.seq_len-1][2] # (num_stock)
+        if self.mode == "convert":
+            quantity_price_feature = convert_nan_inf(quantity_price_feature)
+            fundamental_feature = convert_nan_inf(fundamental_feature)
+            label = convert_nan_inf(label)
+        elif self.mode == "drop":
+            quantity_price_feature, mask = drop_nan_inf(quantity_price_feature)
+            fundamental_feature = fundamental_feature[mask, :]
+            label = label[mask]
+        return quantity_price_feature, fundamental_feature, label
+
+class RandomSampleSampler(Sampler):
+    def __init__(self, data_source:Dataset, num_samples_per_epoch:int):
+        self.data_source:Dataset = data_source
+        self.num_samples_per_epoch:int = num_samples_per_epoch
+
+    def __iter__(self):
+        indices = random.sample(range(len(self.data_source)), self.num_samples_per_epoch)
+        return iter(indices)
+
+    def __len__(self):
+        return self.num_samples_per_epoch
+
+class RandomBatchSampler(Sampler):
+    def __init__(self, data_source:Dataset, num_batches_per_epoch:int, batch_size:int):
+        self.data_source:Dataset = data_source
+        self.num_batches_per_epoch:int = num_batches_per_epoch
+        self.batch_size:int = batch_size
+
+    def __iter__(self):
+        # 计算总的batch数量
+        num_total_batches = len(self.data_source) // self.batch_size
+        # 随机选择 num_batches_per_epoch 个批次的起始索引
+        selected_batches = random.sample(range(num_total_batches), self.num_batches_per_epoch)
+        # 生成这些批次的所有索引
+        indices = []
+        for batch_idx in selected_batches:
+            start_idx = batch_idx * self.batch_size
+            indices.extend(range(start_idx, start_idx + self.batch_size))
+        return iter(indices)
+
+    def __len__(self):
+        return self.num_batches_per_epoch * self.batch_size
+
+# TODO
+def parse_args():
+    parser = argparse.ArgumentParser(description="Data acquisition and dataset generation.")
+
+    parser.add_argument("--log_folder", type=str, default=os.curdir, help="Path of folder for log file. Default `.`")
+    parser.add_argument("--log_name", type=str, default="log.txt", help="Name of log file. Default `log.txt`")
+
+    parser.add_argument("--quantity_price_feature_dir", type=str, required=True, help="Path of folder for quantity-price feature data files")
+    parser.add_argument("--fundamental_feature_dir", type=str, required=True, help="Path of folder for fundamental feature data files")
+    parser.add_argument("--label_dir", type=str, required=True, help="Path of folder for label data files")
+
+    parser.add_argument("--file_format", type=str, default="pkl", help="File format to read, literally `csv`, `pkl`, `parquet` or `feather`. Default `pkl`")
+    parser.add_argument("--label_name", type=str, required=True, help="Target label name (col name in y files)")
+
+    parser.add_argument("--split_ratio", type=float, nargs=3, default=[0.7, 0.2, 0.1], help="Split ratio for train-validation-test. Default 0.7, 0.2, 0.1")
+    parser.add_argument("--mask_len", type=int, default=0, help="Mask seq length to avoid model cheat(get information from future), e.g. mask 20 days when training prediction for ret20. Default 0")
+    parser.add_argument("--mode", type=str, default="convert", help="Mode of processing missing value and infinite value. Literally `convert`(convert Nan to 0 and Inf to the greatest finite value representable by input’s dtype) or `drop`(drop stock code containing Nan or Inf in the whole sequence). Default `convert`")
+    parser.add_argument("--dtype", type=str, default="FP32", help="Dtype of data tensor. Literally `FP32`, `FP64`, `FP16` or `BF16`. Default `FP32`")
+    parser.add_argument("--train_seq_len", type=int, required=True, help="Sequence length (num of days) for train dataset")
+    parser.add_argument("--val_seq_len", type=int, default=None, help="Sequence length (num of days) for validation dataset. If not specified, default equal to train_seq_len.")
+    parser.add_argument("--test_seq_len", type=int, default=None, help="Sequence length (num of days) for test dataset. If not specified, default equal to train_seq_len.")
+
+    parser.add_argument("--save_path", type=str, required=True, help="Path to save the dataset dictionary.")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    log_folder = args.log_folder
+    log_name = args.log_name
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+    
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - [%(levelname)s] : %(message)s',
+        handlers=[logging.FileHandler(os.path.join(log_folder, log_name)), logging.StreamHandler()])
+    
+    logging.debug(f"Command: {' '.join(sys.argv)}")
+    logging.debug(f"Params: {vars(args)}")
+
+    dataset = StockDataset(quantity_price_feature_dir=args.quantity_price_feature_dir,
+                           fundamental_feature_dir=args.fundamental_feature_dir,
+                           label_dir=args.label_dir,
+                           label_name=args.label_name, 
+                           format=args.file_format,
+                           dtype=args.dtype)
+    train_set, val_set, test_set = dataset.serial_split(ratios=args.split_ratio, mask=args.mask_len)
+    train_set = StockSequenceDataset(train_set, seq_len=args.train_seq_len, mode=args.mode)
+    val_set = StockSequenceDataset(val_set, seq_len=args.val_seq_len or args.train_seq_len, mode=args.mode)
+    test_set = StockSequenceDataset(test_set, seq_len=args.test_seq_len or args.train_seq_len, mode=args.mode)
+    logging.debug(f"train_set length: {len(train_set)}, val_set length: {len(val_set)}, test_set length: {len(test_set)}")
+
+    torch.save({"train": train_set, "val": val_set, "test": test_set}, args.save_path)
+    logging.debug(f"Dataset saved to {args.save_path}")
+    print(train_set[0][1].shape)
+
+# python dataset.py --x_folder "D:\PycharmProjects\SWHY\data\preprocess\alpha" --y_folder "D:\PycharmProjects\SWHY\data\preprocess\label" --label_name "ret10" --train_seq_len 20 --save_path "D:\PycharmProjects\SWHY\data\preprocess\dataset.pt"
+
+# python dataset.py --x_folder "D:\PycharmProjects\SWHY\data\preprocess\alpha_cs_zscore" --y_folder "D:\PycharmProjects\SWHY\data\preprocess\label" --label_name "ret10" --train_seq_len 20 --save_path "D:\PycharmProjects\SWHY\data\preprocess\dataset_cs_zscore.pt"
+
+# python dataset.py --x_folder "D:\PycharmProjects\SWHY\data\preprocess\alpha" --y_folder "D:\PycharmProjects\SWHY\data\preprocess\label" --label_name "ret10" --train_seq_len 20 --save_path "D:\PycharmProjects\SWHY\data\preprocess\dataset.pt" --mask_len 10
+
+# python dataset.py --quantity_price_feature_dir "preprocess\quantity_price_feature" --fundamental_feature_dir "preprocess\fundamental_feature" --label_dir "preprocess\label" --label_name "ret10" --train_seq_len 20 --save_path "dataset.pt" --mask_len 10 --mode "drop"
+
+
+
+
+
---
+++ eval.py
@@ -0,0 +1,229 @@
+import os
+import sys
+import datetime
+import logging
+import argparse
+from numbers import Number
+from typing import List, Dict, Tuple, Optional, Literal, Union, Any, Callable
+
+from tqdm import tqdm
+from safetensors.torch import save_file, load_file
+import numpy as np
+from scipy.stats import pearsonr, spearmanr
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, Sampler
+from torch.utils.tensorboard.writer import SummaryWriter
+
+from matplotlib import pyplot as plt
+import plotly.graph_objs as go
+
+from dataset import StockDataset, StockSequenceDataset
+from nets import AttnFactorVAE
+from loss import ObjectiveLoss, MSE_Loss, KL_Div_Loss, PearsonCorr, SpearmanCorr
+from utils import str2bool
+
+
+class FactorVAEEvaluator:
+    def __init__(self,
+                 model:AttnFactorVAE,
+                 device:torch.device=torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")) -> None:
+        
+        self.model:AttnFactorVAE = model # FactorVAE 模型实例
+        self.test_loader:DataLoader
+        
+        self.pred_eval_func:Union[nn.Module, Callable]
+        self.latent_eval_func:Union[nn.Module, Callable] = KL_Div_Loss()
+        self.pred_scores:List[float] = []
+        self.latent_scores:List[float] = []
+        
+        self.y_true_list:List[torch.Tensor] = []
+        self.y_hat_list:List[torch.Tensor] = []
+        self.y_pred_list:List[torch.Tensor] = []
+        
+        self.log_folder:str = "log"
+        self.device = device # 运算设备，默认为 CUDA（如果可用，否则为CPU）
+
+        self.save_folder:str = "."
+        self.plotter = Plotter()
+    
+    def load_dataset(self, test_set:StockSequenceDataset, num_workers:int = 4):
+        self.test_loader = DataLoader(dataset=test_set,
+                                        batch_size=None, 
+                                        shuffle=False,
+                                        num_workers=num_workers)
+
+    def load_checkpoint(self, model_path:str):
+        if model_path.endswith(".pt"):
+            self.model.load_state_dict(torch.load(model_path))
+        elif model_path.endswith(".safetensors"):
+            self.model.load_state_dict(load_file(model_path))
+    
+    def calculate_icir(self, ic_list:List[float]):
+        ic_mean = np.mean(ic_list)
+        ic_std = np.std(ic_list, ddof=1)  # Use ddof=1 to get the sample standard deviation
+        n = len(ic_list)
+    
+        if ic_std == 0:
+            return float('inf') if ic_mean != 0 else 0
+        
+        icir = (ic_mean / ic_std) * np.sqrt(n)
+        return icir
+    
+    def eval(self, metric:Literal["MSE", "IC", "Rank_IC", "ICIR", "Rank_ICIR"]="IC"):
+        if metric == "MSE":
+            self.pred_eval_func = MSE_Loss(scale=1)
+        elif metric == "IC" or "ICIR":
+            self.pred_eval_func = PearsonCorr()
+        elif metric == "Rank_IC" or "Rank_ICIR":
+            self.pred_eval_func = SpearmanCorr()
+        
+        self.eval_scores = []
+        model = self.model.to(device=self.device)
+        model.eval() # set eval mode to frozen layers like dropout
+        with torch.no_grad(): 
+            for batch, (quantity_price_feature, fundamental_feature, label) in enumerate(tqdm(self.test_loader)):
+                quantity_price_feature = quantity_price_feature.to(device=self.device)
+                fundamental_feature = fundamental_feature.to(device=self.device)
+                label = label.to(device=self.device)
+                y_pred, mu_posterior, sigma_posterior, mu_prior, sigma_prior = model(fundamental_feature, 
+                                                                                     quantity_price_feature, 
+                                                                                     label)
+                y_hat, mu_prior, sigma_prior  = model.predict(fundamental_feature, 
+                                                              quantity_price_feature,)
+                pred_score = self.pred_eval_func(y_hat, label)
+                latent_score = self.latent_eval_func(mu_prior, sigma_prior, mu_posterior, sigma_posterior)
+                
+                self.pred_scores.append(pred_score.item())
+                self.latent_scores.append(latent_score.item())
+                
+                self.y_true_list.append(label)
+                self.y_hat_list.append(y_hat)
+                self.y_pred_list.append(y_pred)
+        if metric == "MSE" or "IC" or "Rank_IC":
+            y_pred_score = sum(self.pred_scores) / len(self.pred_scores)
+        elif metric == "ICIR" or "Rank_ICIR":
+            y_pred_score = self.calculate_icir(self.pred_scores)
+        latent_kl_div = sum(self.latent_scores) / len(self.latent_scores)
+        logging.info(f"y pred score: {y_pred_score}")
+        logging.info(f"latent kl divergence: {latent_kl_div}")
+    
+    def visualize(self, idx:int=0, save_folder:Optional[str]=None):
+        if save_folder is not None:
+            self.save_folder = save_folder
+        self.plotter.plot_score(self.pred_scores, 
+                                self.latent_scores)
+        self.plotter.save_fig(os.path.join(self.save_folder, "Scores"))
+        self.plotter.plot_pred_sample(self.y_true_list, 
+                                      self.y_hat_list, 
+                                      self.y_pred_list,
+                                      idx=idx)
+        self.plotter.save_fig(os.path.join(self.save_folder, f"Trace {idx}"))
+
+class Plotter:
+    def __init__(self) -> None:
+        pass
+    
+    def plot_score(self, pred_scores, latent_scores):
+        plt.figure(figsize=(10, 6))
+        plt.plot(pred_scores, label='pred scores', marker='', color="b")
+        plt.plot(latent_scores, label='latent scores', marker='', color="r")
+
+        plt.legend()
+        plt.title('Evaluation Scores')
+        plt.xlabel('Index')
+        plt.ylabel('Value')
+    
+    def plot_pred_sample(self, y_true_list, y_hat_list, y_pred_list, idx=0):
+        y_true_list = [y_true[idx].item() for y_true in y_true_list]
+        y_hat_list = [y_hat[idx].item() for y_hat in y_hat_list]
+        y_pred_list = [y_pred[idx].item() for y_pred in y_pred_list]
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(y_true_list, label='y true', marker='', color="g")
+        plt.plot(y_hat_list, label='y rec', marker='', color="b")
+        plt.plot(y_pred_list, label='y pred', marker='', color="r")
+
+        plt.legend()
+        plt.title('Comparison of y_true, y_hat, and y_pred')
+        plt.xlabel('Index')
+        plt.ylabel('Value')
+
+    def save_fig(self, filename:str):
+        if not filename.endswith(".png"):
+            filename = filename + ".png"
+        plt.savefig(filename)
+def parse_args():
+    parser = argparse.ArgumentParser(description="FactorVAE Training.")
+
+    parser.add_argument("--log_folder", type=str, default=os.curdir, help="Path of folder for log file. Default `.`")
+    parser.add_argument("--log_name", type=str, default="log.txt", help="Name of log file. Default `log.txt`")
+
+    parser.add_argument("--dataset_path", type=str, required=True, help="Path of dataset .pt file")
+    parser.add_argument("--subset", type=str, default="test", help="Subset of dataset, literally `train`, `val` or `test`. Default `test`")
+    parser.add_argument("--checkpoint_path", type=str, required=True, help="Path of checkpoint")
+
+    parser.add_argument("--quantity_price_feature_size", type=int, required=True, help="Input size of quantity-price feature")
+    parser.add_argument("--fundamental_feature_size", type=int, required=True, help="Input size of fundamental feature")
+    parser.add_argument("--num_gru_layers", type=int, required=True, help="Num of GRU layers in feature extractor.")
+    parser.add_argument("--gru_hidden_size", type=int, required=True, help="Hidden size of each GRU layer. num_gru_layers * gru_hidden_size i.e. the input size of FactorEncoder and Factor Predictor.")
+    parser.add_argument("--hidden_size", type=int, required=True, help="Hidden size of FactorVAE(Encoder, Pedictor and Decoder), i.e. num of portfolios.")
+    parser.add_argument("--latent_size", type=int, required=True, help="Latent size of FactorVAE(Encoder, Pedictor and Decoder), i.e. num of factors.")
+    parser.add_argument("--std_activation", type=str, default="exp", help="Activation function for standard deviation calculation, literally `exp` or `softplus`. Default `exp`")
+    
+    parser.add_argument("--num_workers", type=int, default=4, help="Num of subprocesses to use for data loading. 0 means that the data will be loaded in the main process. Default 4")
+    parser.add_argument("--metric", type=str, default="IC", help="Eval metric type, literally `MSE`, `IC`, `Rank_IC`, `ICIR` or `Rank_ICIR`. Default `IC`. ")
+
+    parser.add_argument("--visualize", type=str2bool, default=True, help="Whether to shuffle dataloader. Default True")
+    parser.add_argument("--index", type=int, default=0, help="Stock index to plot Comparison of y_true, y_hat, and y_pred. Default 0")
+    parser.add_argument("--save_folder", type=str, default=None, help="Folder to save plot figures")
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    os.makedirs(args.log_folder, exist_ok=True)
+    os.makedirs(args.save_folder, exist_ok=True)
+    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+    os.environ["TF_ENABLE_ONEDNN_OPTS"] = 0
+    
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - [%(levelname)s] : %(message)s',
+        handlers=[logging.FileHandler(os.path.join(args.log_folder, args.log_name)), logging.StreamHandler()])
+    
+    logging.debug(f"Command: {' '.join(sys.argv)}")
+    logging.debug(f"Params: {vars(args)}")
+    
+    datasets:Dict[str, StockSequenceDataset] = torch.load(args.dataset_path)
+    test_set = datasets[args.subset]
+
+    model = AttnFactorVAE(fundamental_feature_size=args.fundamental_feature_size, 
+                          quantity_price_feature_size=args.quantity_price_feature_size,
+                          num_gru_layers=args.num_gru_layers, 
+                          gru_hidden_size=args.gru_hidden_size, 
+                          hidden_size=args.hidden_size, 
+                          latent_size=args.latent_size,
+                          gru_drop_out=0,
+                          std_activ=args.std_activation)
+    
+    evaluator = FactorVAEEvaluator(model=model)
+    evaluator.load_checkpoint(args.checkpoint_path)
+    evaluator.load_dataset(test_set, num_workers=args.num_workers)
+    
+    evaluator.eval(metric=args.metric)
+    if args.visualize:
+        evaluator.visualize(idx=args.index, save_folder=args.save_folder)
+        
+
+
+
+
+                    
+            
+                
+
+    
+
